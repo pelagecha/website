dfcfdffda007925fca36dd0e9eda0041d55f8a0a
amency <nikitapelagecha@gmail.com>
2024-09-16 11:17:57 +0100

    trying to add mainview

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [], // No external domains since we're using local images exclusively
+        domains: ["picsum.photos"], // No external domains since we're using local images exclusively
     },
     reactStrictMode: true,
     // Add other configurations if necessary
