083316d1aaf68ef4dfff743c2fa0bdb26b3721f7
amency <nikitapelagecha@gmail.com>
2024-09-19 15:56:16 +0100

    fixed a few issues

--- app/components/Blogs.tsx
+++ app/components/Blogs.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect, useRef } from "react";
 import { blogsData } from "../data/blogsData";
 import BlogCard from "./BlogCard";
--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { Project } from "../data/projectsData";
 import Link from "next/link";
