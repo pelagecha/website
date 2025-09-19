afb9d7ec9895699a4c9b87215641fa303ee74381
amency <nikitapelagecha@gmail.com>
2025-09-19 13:06:19 +0100

    fixed broken about and products links

---
+++ app/about/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import ParticlesBackground from "@/app/components/ParticlesBackground";
+
+const AboutPage: React.FC = () => {
+    return (
+        <div className="relative flex items-center justify-center">
+            <ParticlesBackground />
+            <main className="container mx-auto px-4 py-24 relative z-10 text-center">
+                <h1 className="text-5xl font-bold text-black dark:text-white">
+                    About Me
+                </h1>
+                <p className="mt-4 text-lg text-gray-700 dark:text-gray-300 font-semibold">
+                    Hi! I&apos;m Nikita Pelagecha. I build software and train
+                    deep neural networks.
+                </p>
+                <p className="mt-2 text-lg text-gray-600 dark:text-gray-400">
+                    This website was built using Next.js, React, framer-motion
+                    and Tailwind CSS.
+                </p>
+                <div className="flex justify-center mt-8 space-x-4">
+                    <Link href="/" passHref>
+                        <motion.button
+                            className="px-6 py-2 rounded-md shadow-md bg-blue-400 text-black hover:bg-blue-500 dark:bg-blue-600 dark:text-white dark:hover:bg-blue-700"
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale: 0.95 }}
+                        >
+                            Home
+                        </motion.button>
+                    </Link>
+                    <motion.a
+                        href="mailto:nikitapelagecha@gmail.com"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="px-6 py-2 rounded-md shadow-md bg-purple-400 text-black hover:bg-purple-500 dark:bg-purple-600 dark:text-white dark:hover:bg-purple-700"
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.95 }}
+                    >
+                        Contact
+                    </motion.a>
+                </div>
+            </main>
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ app/products/page.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import LinkCard from "@/app/components/Extensions";
+import ParticlesBackground from "@/app/components/ParticlesBackground";
+import Link from "next/link";
+
+const Products = () => {
+    return (
+        <div className="relative flex flex-col">
+            <div className="absolute inset-0 z-0">
+                <ParticlesBackground />
+            </div>
+            <div className="container mx-auto my-16 p-4 z-10">
+                <div className="container mx-auto p-4">
+                    <h1 className="text-2xl font-bold mb-4">
+                        Explore some of the extensions I wrote
+                    </h1>
+                    <LinkCard
+                        href="https://marketplace.visualstudio.com/items?itemName=pelagecha.4u2c"
+                        title="4u2c VSCode light theme"
+                    />
+                </div>
+            </div>
+            <div className="flex flex-col items-center mt-8 space-y-4 relative z-10">
+                <Link
+                    href="/"
+                    className="inline-block bg-transparent border border-gray-500 text-gray-800 dark:text-gray-200 font-semibold py-2 px-4 rounded hover:bg-gray-100 dark:hover:bg-gray-800 hover:bg-opacity-25 transition ease-in-out duration-200"
+                >
+                    Back to Main Page
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Products;
